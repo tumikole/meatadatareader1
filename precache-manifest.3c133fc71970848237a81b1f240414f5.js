1afb7f4b140907dd7850071b98086e482e4087ce
tumikole <samuelramollo@gmail.com>
2022-01-20 10:52:17 +0200

    Updates

---
+++ precache-manifest.3c133fc71970848237a81b1f240414f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ff0e1c504593ffd2a50819e01e4136",
+    "url": "/meatadatareader1/index.html"
+  },
+  {
+    "revision": "5e0930f3dd6a9b3d908a",
+    "url": "/meatadatareader1/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "652276cf891b440da3e4",
+    "url": "/meatadatareader1/static/css/main.6c19a417.chunk.css"
+  },
+  {
+    "revision": "5e0930f3dd6a9b3d908a",
+    "url": "/meatadatareader1/static/js/2.b13ba40d.chunk.js"
+  },
+  {
+    "revision": "356ab6c8494ed7734735",
+    "url": "/meatadatareader1/static/js/3.d56735c2.chunk.js"
+  },
+  {
+    "revision": "652276cf891b440da3e4",
+    "url": "/meatadatareader1/static/js/main.d58a8ebb.chunk.js"
+  },
+  {
+    "revision": "5acc58979ad44e9c91fa",
+    "url": "/meatadatareader1/static/js/runtime~main.cad15ef3.js"
+  }
+]);
